44fb2daaa7937bb62d33987f136cad75720db926
mm <mm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2010-01-13 16:53:08 +0000

    iplug-sns: - added API gazetteerService.getLocationsFromText()

--- src/main/java/de/ingrid/external/GazetteerService.java
+++ src/main/java/de/ingrid/external/GazetteerService.java
@@ -49,23 +49,29 @@ public interface GazetteerService {
      * @param matching occurence of the query term during search. Supports exact match, beginning of word, and contained.
      * @param language language of queryTerm
      * @return Array of Locations found for queryTerm (or empty array).
+     * 		NOTICE: SNS knows "expired" locations. These ones are not returned !
      */
     Location[] findLocationsFromQueryTerm(String queryTerm, QueryType typeOfQuery,
     		MatchingType matching, Locale language);
 
     /**
-     * Analyze arbitrary TEXT delivering gazetteer locations.
-     * When using SNS the autoClassify method of SNS is used.<br/>
-     * <ul><li>used in Portal Extended Search for look up of locations from arbitrary
+     * Classify a text meaning identify gazetteer locations describing the text.
+     * When using SNS the autoclassify method of SNS is used.<br/>
+     * <ul>
+     * <li>used in Portal Extended Search for look up of locations from arbitrary
      * entered text see<br/>
      * PortalU: http://localhost:8080/ingrid-portal/portal/search-extended/search-ext-env-place-geothesaurus.psml
      * <br/>Enter text and click "Look-up Geographic Names"
+     * <li>when using SNS here's the method called: http://www.semantic-network.de/doc_autoclassifytext.html?lang=en
+     * </br>NOTICE: not all parameters are part of UI.
      * </ul>
-     * @param text arbitrary text to classify. Multiple words, sentences etc.
-     * @param analyzeMaxWords The maximum number of words to analyze
+     * @param text any kind of text to classify
+     * @param analyzeMaxWords The maximal number of words to analyze.
+     * 		If the document contains more words these will be ignored
      * @param ignoreCase Set to true to ignore capitalization of the text
-     * @param language language of text
+     * @param language language of the text and the results.
      * @return Array of Locations found for text (or empty array).
+     * 		NOTICE: SNS knows "expired" locations. These ones are not returned !
      */
     Location[] getLocationsFromText(String text, int analyzeMaxWords, boolean ignoreCase, Locale language);
 
@@ -80,6 +86,7 @@ public interface GazetteerService {
      * @param language request results in this language. If passed language can't be processed
      * 		or is null then default language may be used (PortalU: de, GS Soil: en) 
      * @return Array of related locations found for passed location (or empty array)
+     * 		NOTICE: SNS knows "expired" locations. These ones are not returned !
      */
     Location[] getRelatedLocationsFromLocation(String locationId, Locale language);
 }
